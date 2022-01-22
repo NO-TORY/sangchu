29b6b19d1f368bce93ec0e8b776222d21611143d
0x6c7 <97148865+oxsixcseven@users.noreply.github.com>
2022-01-22 17:28:18 +0900

    Update youtube.py

--- youtube.py
+++ youtube.py
@@ -1,29 +1,23 @@
 from requests import get
 from pymongo import MongoClient
-from extensions import DictSerializerMixin
+from bson import ObjectId
 
-dbclient = MongoClient("A MONGODB URL")["dddd"]["dddd"]
+class LastVideo:
+    database = MongoClient(A STRING MONGO-DB URL)
 
-class LastVideo(DictSerializerMixin):
-    def __init__(self, id: str, api_key: str, **kwargs):
-        __slots__ = ("last_video_url", "old_last_video", "id", "api_key")
-        super().__init__(**kwargs)
+    def __init__(self, id: str, api_key: str):
         self.id = id
         self.api_key = api_key
         self.old_last_video = self.get_old()
-        self.last_video_url = self.get()
+        self.last_video = self.get()
 
     def get(self):
         channel = "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=%s&maxResults=10&order=date&type=video&key=%s"%(self.id, self.api_key)
-
         lastVideo = get(channel).json()["items"][0]
-
         return "https://www.youtube.com/watch?v=%s"%lastVideo["id"]["videoId"]
 
     def update(self):
-        global dbclient
-        return dbclient.update_one({"_id": "61e2df1d5b282eb1e1ee12b0"}, {"$set": {"new_video": self.last_video_url}})
+        return self.database["dddd"]["dddd"].update_one({"_id": ObjectId("61e2df1d5b282eb1e1ee12b0")}, {"$set": {"new_video": self.last_video}})
 
     def get_old(self):
-        global dbclient
-        return dbclient.find_one()
+        return self.database["dddd"]["dddd"].find_one()
